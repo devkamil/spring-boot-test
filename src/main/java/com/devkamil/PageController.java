a565543e5a4d43d6ad9f58bc07babe7be3cfdbd6
devkamil <kamilkarwowski.dev@gmail.com>
2017-07-29 23:42:22 +0200

    Add pom.xml, add Controller class and Main class

---
+++ src/main/java/com/devkamil/PageController.java
@@ -0,0 +1,23 @@
+package com.devkamil;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String mainPage() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String pageTwo() {
+		return "Hi!";
+	}
+	
+
+}
