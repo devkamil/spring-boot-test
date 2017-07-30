a211fde41cbbc5b98d5506a655a531ef44a40a7d
devkamil <kamilkarwowski.dev@gmail.com>
2017-07-30 12:08:11 +0200

    Add 'database-testMethod' to Controller

--- src/main/java/com/devkamil/PageController.java
+++ src/main/java/com/devkamil/PageController.java
@@ -1,11 +1,18 @@
 package com.devkamil;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import pl.devkamil.Task;
+
 @Controller
 public class PageController {
+	
+	@Autowired
+	public pl.devkamil.TaskRepository taskRepository;
+	
 	
 	@RequestMapping("/")
 	@ResponseBody
@@ -19,5 +26,24 @@ public class PageController {
 		return "Hi!";
 	}
 	
+	
+	@RequestMapping("/db")
+	@ResponseBody
+	public String testMethod(){
+		StringBuilder response = new StringBuilder();
+		
+		Task task = new Task()
+				.withName("Task 1")
+				.withDescription("Test task")
+				.withBudget(123.55)
+				.withDone(true);
+		taskRepository.save(task);
+		
+		for(Task i: taskRepository.findAll()) {
+			response.append(i).append("<br>");
+		}
+		
+		return response.toString();
+	}
 
 }
--- src/main/java/pl/devkamil/Main.java
+++ src/main/java/pl/devkamil/Main.java
@@ -3,10 +3,12 @@ package pl.devkamil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EnableAutoConfiguration
 @ComponentScan(basePackageClasses = com.devkamil.PageController.class)
+@EnableJpaRepositories(basePackageClasses = TaskRepository.class)
 public class Main {
 	
 	public static void main(String[] args) {
